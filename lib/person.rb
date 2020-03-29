7b4084430a82b7a88cfd75bb2c531f808d820a5d
Mariam Haider <mfh.haider@gmail.com>
2020-03-29 00:01:00 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,12 @@
 class Person
-  def initialize(name)
-    @name = name
-  end 
-end 
+  def initialize (persons_name)
+    @name = persons_name
+  end
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
