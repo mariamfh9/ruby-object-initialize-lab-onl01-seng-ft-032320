bfe6e831856d6ac1024ff1a417b88a1d8adc0413
Mariam Haider <mfh.haider@gmail.com>
2020-03-29 00:01:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,12 @@
-class Dog
-  def initialize(breed)
-    @breed = breed
+def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+
+  def name
+    @name
+  end
+  def breed
+    @breed
   end 
 end 
